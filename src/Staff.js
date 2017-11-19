998a63b52c511076e46a5bcc6d90b9c98ba1862d
LENOVO-PC\Lenovo <RemGYH@sjtu.edu.cn>
2017-11-19 15:17:53 +0800

    增加Staff类模块化，将rawdata移到staff.js中（理解了大致结构和各组件内容）bundle.js已经生成，移除webpack的相关配置文件和依赖配置文件

---
+++ src/Staff.js
@@ -0,0 +1,44 @@
+class staffItem {
+    constructor(item){
+        this.info = {};
+        this.info.name = item.name;
+        this.info.age = item.age || 0;
+        this.info.sex = item.sex;
+        this.info.id = item.id;
+        this.info.descrip = item.descrip || '';
+        this.key = ++staffItem.key;
+    }
+}
+staffItem.key = 0;
+
+export default class Staff {
+
+    constructor(){
+        this.allStaff = [
+            new staffItem(Staff.rawData[0]),
+            new staffItem(Staff.rawData[1]),
+            new staffItem(Staff.rawData[2]),
+            new staffItem(Staff.rawData[3]),
+            new staffItem(Staff.rawData[4]),
+            new staffItem(Staff.rawData[5]),
+            new staffItem(Staff.rawData[6]),
+            new staffItem(Staff.rawData[7]),
+            new staffItem(Staff.rawData[8]),
+            new staffItem(Staff.rawData[9]),
+            new staffItem(Staff.rawData[10])
+        ];
+        this.staff = this.allStaff;
+    }
+}
+
+Staff.rawData = [{ descrip:'我是一匹来自远方的狼。', sex: '男', age: 20, name: '张三', id: '主任'},
+    { descrip:'我是一匹来自远方的狼。', sex: '女', age: 21, name: '赵静', id: '学生'},
+    { descrip:'我是一匹来自远方的狼。', sex: '女', age: 22, name: '王二麻', id: '学生'},
+    { descrip:'我是一匹来自远方的狼。', sex: '女', age: 24, name: '李晓婷', id: '实习'},
+    { descrip:'我是一匹来自远方的狼。', sex: '男', age: 23, name: '张春田', id: '实习'},
+    { descrip:'我是一匹来自远方的狼。', sex: '男', age: 22, name: '刘建国', id: '学生'},
+    { descrip:'我是一匹来自远方的狼。', sex: '男', age: 24, name: '张八', id: '主任'},
+    { descrip:'我是一匹来自远方的狗。', sex: '男', age: 35, name: '李四', id: '老师'},
+    { descrip:'我是一匹来自远方的猪。', sex: '男', age: 42, name: '王五', id: '学生'},
+    { descrip:'我是一匹来自远方的牛。', sex: '男', age: 50, name: '赵六', id: '实习'},
+    { descrip:'我是一匹来自远方的马。', sex: '男', age: 60, name: '孙七', id: '实习'}];
